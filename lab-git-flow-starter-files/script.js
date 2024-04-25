05b6850fbd2c5b5cd23f4cfc3365ad19a553167d
Dina Shouhdy <dshouhdy@gmail.com>
2024-04-24 17:19:53 -0700

    added dina's poem

--- lab-git-flow-starter-files/script.js
+++ lab-git-flow-starter-files/script.js
@@ -13,7 +13,12 @@ const poems = [
     author: "Ernie",
     poem: "Roses are red \n Violets are blue \n Jerry Seinfeld is considering \n Making Bee Movie 2",
     image: "bee.jpeg",
-  },  
+  }, 
+  {
+    author: "Dina",
+    poem: "I'm hungry \n Give me food \n Your food looks good Dakota",
+    image: "bee.jpeg",
+  }, 
 ];
 
 let displayPoems = (array) => {
